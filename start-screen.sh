f01b3cac370f7b0f4278d1fe9b950278e989e6c7
Isaac <isaac@isaacclarke.com>
2025-03-08 16:34:10 +0800

    Update start-screen.sh

--- start-screen.sh
+++ start-screen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt update
 apt install -y screen
+export HF_HOME=/workspace/.cache
 screen -S workers -dm bash -c './run-workers.sh; exec bash'
 screen -S server -dm bash -c './run-server.sh; exec bash'
